b1d1b45f314976473b7a7d2c8f7602243cd19830
Jhonatas Rodrigues <jhonatasrm@gmail.com>
2019-04-09 15:16:33 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+[<img align="right" src="https://img.shields.io/github/issues/mozillabrasil/sumo_live_helper.svg">](https://img.shields.io/github/issues/mozillabrasil/sumo_live_helper.svg)
+[<img align="right" src="https://img.shields.io/github/license/mozillabrasil/sumo_live_helper.svg">](https://img.shields.io/github/license/mozillabrasilo/sumo_live_helper.svg)
+[<img align="right" src="https://img.shields.io/github/forks/mozillabrasil/sumo_live_helper.svg">](https://img.shields.io/github/forks/mozillabrasil/sumo_live_helper.svg)
+[<img align="right" src="https://img.shields.io/github/stars/mozillabrasil/sumo_live_helper.svg">](https://img.shields.io/github/stars/mozillabrasil/sumo_live_helper.svg)
+[<img align="right" src="https://img.shields.io/github/release/mozillabrasil/sumo_live_helper.svg">](https://img.shields.io/github/release/mozillabrasil/sumo_live_helper.svg)
+
+![sumo_live_helper](src/res/icons/icon.png)
+# SUMO Live Helper
+
+Helper Add-on for forum contributors:
+
+- notification for new incoming questions in the support forum for selected products/languages
+- notification for contributors about updates in threads where they have participated before
+- set keywords in new questions that will cause an alert in any case
+- shows a notification/thank you note when your solution count increases (only works while you're logged in to SUMO)
+- shows some statistics about your account when you hover over the toolbar icon (only while you're logged in to SUMO)
+
+## Requirements
+
+* Firefox 58.0 or later
+
+## How to test this extension
+
+1. Clone this repository git clone ```https://github.com/mozillabrasil/sumo_live_helper.git```
+
+2. In the Firefox address bar type ```about:debugging```
+
+3. Click ```Load Temporary Add-on```, a dialog window opens.
+
+4. In the cloned repository, select the ```manifest.json``` file. The webextension icon will appear in your toolbar.
+
+## How Contribute
+
+Have a suggestion, doubt or encountered an error? Please open a new [issue](https://github.com/mozillabrasil/sumo_live_helper/issues).
+
+## Learn More
+To learn more about webextensions development, see this [Mozilla developer Network documentation](https://developer.mozilla.org/en-US/Add-ons/WebExtensions).
+
+### License
+This repository is available under the [Mozilla Public License 2.0](https://github.com/mozillabrasil/sumo_live_helper/blob/master/LICENSE)
+
---
+++ src/js/preferences.js
@@ -0,0 +1,169 @@
+var backgroundPage = browser.extension.getBackgroundPage();
+
+// notify_keywords
+$(document).ready(function(){
+  var radios = document.getElementsByName("notify_involved");
+  var val = localStorage.getItem('notify_involved');
+  for(var i=0;i<radios.length;i++){
+    if(radios[i].value == val){
+      radios[i].checked = true;
+    }
+  }
+$('input[name="notify_involved"]').on('change', function(){
+    localStorage.setItem('notify_involved', $(this).val());
+    backgroundPage.request.onload();
+  });
+});
+
+// notify_keywords
+$(document).ready(function(){
+  var radios = document.getElementsByName("notify_keywords");
+  var val = localStorage.getItem('notify_keywords');
+  for(var i=0;i<radios.length;i++){
+    if(radios[i].value == val){
+      radios[i].checked = true;
+    }
+  }
+$('input[name="notify_keywords"]').on('change', function(){
+    localStorage.setItem('notify_keywords', $(this).val());
+    backgroundPage.request.onload();
+   });
+});
+
+// keywords
+$(document).ready(function(){
+  var value = document.getElementsByName("keywords");
+  var val = localStorage.getItem('keywords');
+
+  localStorage.setItem('keywords', value.value);
+   backgroundPage.contextMenuFunction();
+});
+
+// notify_opentab
+$(document).ready(function(){
+  var radios = document.getElementsByName("notify_opentab");
+  var val = localStorage.getItem('notify_opentab');
+  for(var i=0;i<radios.length;i++){
+    if(radios[i].value == val){
+      radios[i].checked = true;
+    }
+  }
+$('input[name="notify_opentab"]').on('change', function(){
+    localStorage.setItem('notify_opentab', $(this).val());
+    backgroundPage.request.onload();
+   });
+});
+
+// watch_en
+$(document).ready(function(){
+  var radios = document.getElementsByName("watch_en");
+  var val = localStorage.getItem('watch_en');
+  for(var i=0;i<radios.length;i++){
+    if(radios[i].value == val){
+      radios[i].checked = true;
+    }
+  }
+$('input[name="watch_en"]').on('change', function(){
+    localStorage.setItem('watch_en', $(this).val());
+    backgroundPage.request.onload();
+   });
+});
+
+// watch_es
+$(document).ready(function(){
+  var radios = document.getElementsByName("watch_es");
+  var val = localStorage.getItem('watch_es');
+  for(var i=0;i<radios.length;i++){
+    if(radios[i].value == val){
+      radios[i].checked = true;
+    }
+  }
+$('input[name="watch_es"]').on('change', function(){
+    localStorage.setItem('watch_es', $(this).val());
+    backgroundPage.request.onload();
+   });
+});
+
+// watch_pt
+$(document).ready(function(){
+  var radios = document.getElementsByName("watch_pt");
+  var val = localStorage.getItem('watch_pt');
+  for(var i=0;i<radios.length;i++){
+    if(radios[i].value == val){
+      radios[i].checked = true;
+    }
+  }
+$('input[name="watch_pt"]').on('change', function(){
+    localStorage.setItem('watch_pt', $(this).val());
+    backgroundPage.request.onload();
+   });
+});
+
+// watch_firefox
+$(document).ready(function(){
+  var radios = document.getElementsByName("watch_firefox");
+  var val = localStorage.getItem('watch_firefox');
+  for(var i=0;i<radios.length;i++){
+    if(radios[i].value == val){
+      radios[i].checked = true;
+    }
+  }
+$('input[name="watch_firefox"]').on('change', function(){
+    localStorage.setItem('watch_firefox', $(this).val());
+    backgroundPage.request.onload();
+   });
+});
+
+// watch_mobile
+$(document).ready(function(){
+  var radios = document.getElementsByName("watch_mobile");
+  var val = localStorage.getItem('watch_mobile');
+  for(var i=0;i<radios.length;i++){
+    if(radios[i].value == val){
+      radios[i].checked = true;
+    }
+  }
+$('input[name="watch_mobile"]').on('change', function(){
+    localStorage.setItem('watch_mobile', $(this).val());
+    backgroundPage.request.onload();
+   });
+});
+
+// watch_thunderbird
+$(document).ready(function(){
+  var radios = document.getElementsByName("watch_thunderbird");
+  var val = localStorage.getItem('watch_thunderbird');
+  for(var i=0;i<radios.length;i++){
+    if(radios[i].value == val){
+      radios[i].checked = true;
+    }
+  }
+$('input[name="watch_thunderbird"]').on('change', function(){
+    localStorage.setItem('watch_thunderbird', $(this).val());
+    backgroundPage.request.onload();
+   });
+});
+
+// watch_webmaker
+$(document).ready(function(){
+  var radios = document.getElementsByName("watch_webmaker");
+  var val = localStorage.getItem('watch_webmaker');
+  for(var i=0;i<radios.length;i++){
+    if(radios[i].value == val){
+      radios[i].checked = true;
+    }
+  }
+$('input[name="watch_webmaker"]').on('change', function(){
+    localStorage.setItem('watch_webmaker', $(this).val());
+    backgroundPage.request.onload();
+   });
+});
+
+// polling
+$(document).ready(function(){
+  var value = document.getElementsByName("polling");
+  var val = localStorage.getItem('polling');
+
+  localStorage.setItem('polling', value.value);
+   backgroundPage.contextMenuFunction();
+});
