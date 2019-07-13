c29d0fb294c851a56c82ddbd113de8f11b956d2b
Jhonatas Rodrigues <jhonatasrm@gmail.com>
2019-07-12 21:38:31 -0300

    Updated Strings + changes the default title of the Add-on

--- src/js/background.js
+++ src/js/background.js
@@ -1,27 +1,26 @@
-var locale = navigator.language;
+let locale = navigator.language;
+let requestAPI = "";
+let request = new XMLHttpRequest();
 var numberOfQuestionsOpened = 0;
 
 setInterval(function () {
     request.onload();
-}, 900000);
+}, 900000); // check every 15 minutes
 
-// click at toolbar icon and open page SUMO
+// clicking the toolbar icon opens the Firefox for Desktop Support Forum page with new questions
 browser.browserAction.onClicked.addListener(openPageSUMO);
 
 function initAPICall() {
-  // request questions opened without any answer
+  // request questions opened without any answer, no locked and no spam
   requestAPI = "https://support.mozilla.org/api/2/question/?format=json&ordering=-id&is_spam=False&is_locked=False&is_taken=False",
   request.open('GET', requestAPI, true);
   request.responseType = 'json';
   request.send();
 }
 
-var requestAPI = "";
-var request = new XMLHttpRequest();
-
 initAPICall();
 
-// fetch the new questions
+// search for new questions
 request.onload = function() {
         var responseSUMO = request.response;
         for(var i = 0; i < 20; i++){
@@ -31,10 +30,18 @@ request.onload = function() {
                 localStorage.setItem('numberOfQuestionsOpened', numberOfQuestionsOpened);
             }
         }
+
         // how many questions
-        console.log("Questions opened "+numberOfQuestionsOpened);
+        console.log("Questions opened = "+numberOfQuestionsOpened);
 
         browser.browserAction.setBadgeText({text: localStorage.getItem('numberOfQuestionsOpened')});
+
+        // changes the title
+        if(localStorage.getItem('numberOfQuestionsOpened') >= 2){
+            browser.browserAction.setTitle({title: localStorage.getItem('numberOfQuestionsOpened')+browser.i18n.getMessage("number_of_questions")});
+        }else{
+            browser.browserAction.setTitle({title: localStorage.getItem('numberOfQuestionsOpened')+browser.i18n.getMessage("one_question")});
+        }
         numberOfQuestionsOpened = 0;
 }
 
@@ -43,6 +50,11 @@ function openPageSUMO() {
     browser.tabs.create({
         url: 'https://support.mozilla.org/'+locale+'/questions/firefox?filter=new'
     });
+
+    // clears the notification of the Add-on icon in the toolbar
     localStorage.setItem('numberOfQuestionsOpened', '');
     browser.browserAction.setBadgeText({text: localStorage.getItem('numberOfQuestionsOpened')});
+
+    // changes the title to default
+    browser.browserAction.setTitle({title: browser.i18n.getMessage("extensionName")});
 }
